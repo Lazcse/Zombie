2b5786259c76ac2db9f2c48fe32723699cda1cad
Laczse <lass738a@ucholstebro.dk>
2021-04-08 09:07:17 +0200

    Forsøg

--- Zombie/Program.cs
+++ Zombie/Program.cs
@@ -6,7 +6,8 @@ namespace Zombie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new Application();
+            app.Run();
         }
     }
 }
